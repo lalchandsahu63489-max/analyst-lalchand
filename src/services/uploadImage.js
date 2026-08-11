4f94d693a657522e775d4ea9c937abfc5675ad99
AmaanKhanGit <amaankhan050606@gmail.com>
2026-08-11 20:16:30 +0530

    upload image function added

--- src/services/uploadImage.js
+++ src/services/uploadImage.js
@@ -1,3 +1,21 @@
-export const uploadImage = async (e)=>{
-  
-}
+export const uploadImage = async (e) => {
+  const file = e.target.files[0];
+
+  const data = new FormData();
+
+  data.append("photos", file);
+  data.append("upload_preset", "portfolio");
+  data.append("cloud_name", "vbju64fk");
+
+  const res = await fetch(
+    "https://api.cloudinary.com/v1_1/vbju64fk/image/upload",
+    {
+      method: "POST",
+      body: data,
+    },
+  );
+
+  const responsedData = res.json();
+
+  return responsedData.url;
+};
