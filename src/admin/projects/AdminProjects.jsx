0517069484a45e9a2464e504ab3653103b6aa492
AmaanKhanGit <amaankhan050606@gmail.com>
2026-08-14 18:58:23 +0530

    unwanted console removed

--- src/admin/projects/AdminProjects.jsx
+++ src/admin/projects/AdminProjects.jsx
@@ -52,7 +52,6 @@ const AdminProjects = () => {
       />
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         {data?.map((project) => {
-          console.log(project);
           return (
             <AdminCard
               key={project.id}
