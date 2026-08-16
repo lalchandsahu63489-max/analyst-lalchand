6aec112adf81fc156beaec91b9880bf15167d137
AmaanKhanGit <amaankhan050606@gmail.com>
2026-08-16 20:27:33 +0530

    important things

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCgOf7nwg1yZ7A_J_cNbb5x5uJxxgkcxO4",
+  authDomain: "portfolio-b46e3.firebaseapp.com",
+  projectId: "portfolio-b46e3",
+  messagingSenderId: "998162094617",
+  appId: "1:998162094617:web:e09068c4ed53de2284a888",
+  measurementId: "G-B4PXH1BETG",
 };
 
 // Initialize Firebase
